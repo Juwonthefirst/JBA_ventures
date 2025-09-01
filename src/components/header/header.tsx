2b64f27a09301887093030382147e6454da4b5d0
juwonthefirst <ajibolajuwon5@gmail.com>
2025-09-01 13:49:59 +0000

    added shadcn

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,6 +2,16 @@ import { useState, type Dispatch, type SetStateAction } from "react";
 import SearchBox from "./search-box.tsx";
 import { type ParamsType } from "../../types.ts";
 import { Menu } from "lucide-react";
+import {
+    Sheet,
+    SheetTrigger,
+    SheetClose,
+    SheetContent,
+    SheetHeader,
+    SheetFooter,
+    SheetTitle,
+    SheetDescription
+} from "@/components/ui/sheet";
 
 interface Props {
     setSearchFilter: Dispatch<SetStateAction<ParamsType>>;
@@ -12,9 +22,10 @@ const Header = ({ setSearchFilter }: Props) => {
         <header className="flex p-2 px-4 absolute top-0 left-0 items-center justify-end w-full bg-white rounded-b-3xl">
             <div className="flex gap-4 items-center">
                 <SearchBox setSearchFilter={setSearchFilter} />
-                <button className="p-2 bg-slate-100 rounded-full">
+                {/*<button className="p-2 bg-slate-100 rounded-full">
                     <Menu />
                 </button>
+                */}
             </div>
         </header>
     );
