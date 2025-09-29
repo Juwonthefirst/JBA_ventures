262a2bf7d91176cba9f3692db93f14964f87ec6b
juwonthefirst <ajibolajuwon5@gmail.com>
2025-09-29 16:23:53 +0100

    fixed onclose

--- src/components/home/search-box.tsx
+++ src/components/home/search-box.tsx
@@ -15,7 +15,7 @@ interface Props {
 const SearchBox = ({ className, searchFilter, setSearchFilter }: Props) => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [inputValue, setInputValue] = useState("");
-  const selectStyling = "w-full **:z-50";
+  const selectStyling = "max-h-7 **:text-xs";
 
   useEffect(() => {
     const timeoutID = setTimeout(() => {
@@ -35,7 +35,7 @@ const SearchBox = ({ className, searchFilter, setSearchFilter }: Props) => {
       <input
         name="search-input"
         placeholder="Search for properties"
-        className="focus:outline-0 w-2/3 lg:w-fit placeholder:italic placeholder:opacity-90"
+        className="focus:outline-0 w-2/3 lg:w-fit placeholder:italic placeholder:opacity-90 text-sm sm:text-base"
         value={inputValue}
         onChange={(event) => {
           setInputValue(event.target.value);
@@ -43,7 +43,7 @@ const SearchBox = ({ className, searchFilter, setSearchFilter }: Props) => {
       />
       <motion.button
         type="button"
-        className=" hover:*:scale-110"
+        className="-m-2 p-1.5 rounded-md hover:bg-black/40 active:bg-black/60 transition-colors"
         animate={{
           rotate: menuOpen ? 90 : undefined,
         }}
@@ -54,21 +54,14 @@ const SearchBox = ({ className, searchFilter, setSearchFilter }: Props) => {
         <Settings2 className="group-has-focus:*:text-accent" />
       </motion.button>
       {menuOpen && (
-        <motion.div
-          className="w-1/2 relative top-0 right-0"
-          initial={{ x: 1200 }}
-          animate={{ x: 0 }}
-          onClose={() => {
-            setMenuOpen(false);
-          }}
-        >
-          <div className=" w-full grid grid-cols-2 /gap-x-8 /md:gap-x-10 /lg:gap-x-12">
+        <div className="absolute -bottom-26 -right-18 z-20 text-black bg-white shadow-md dark:bg-black dark:text-white py-2 px-4 transition-all rounded-md border dark:border-white/20 border-black/80">
+          <div className=" w-full grid grid-cols-2 gap-x-3 gap-y-2">
             <SelectInput
               placeholder="L.G.A"
               className={selectStyling}
               options={lgaJson.Lagos}
               required={false}
-              value={searchFilter.lga}
+              value={searchFilter.lga || ""}
               onChange={(newValue) => {
                 setSearchFilter({ ...searchFilter, lga: newValue });
               }}
@@ -78,7 +71,7 @@ const SearchBox = ({ className, searchFilter, setSearchFilter }: Props) => {
               className={selectStyling}
               options={propertyTypes}
               required={false}
-              value={searchFilter.type}
+              value={searchFilter.type || ""}
               onChange={(newValue) => {
                 setSearchFilter({ ...searchFilter, type: newValue });
               }}
@@ -88,24 +81,13 @@ const SearchBox = ({ className, searchFilter, setSearchFilter }: Props) => {
               className={selectStyling}
               options={propertyOffers}
               required={false}
-              value={searchFilter.offer}
+              value={searchFilter.offer || ""}
               onChange={(newValue) => {
                 setSearchFilter({ ...searchFilter, offer: newValue });
               }}
             />
-
-            <SelectInput
-              placeholder="Type"
-              className={selectStyling}
-              options={propertyTypes}
-              required={false}
-              value={searchFilter.type}
-              onChange={(newValue) => {
-                setSearchFilter({ ...searchFilter, type: newValue });
-              }}
-            />
           </div>
-        </motion.div>
+        </div>
       )}
     </section>
   );
