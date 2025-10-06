f3928c432f29665ba5b75e49165fcca075ca5b5f
juwonthefirst <ajibolajuwon5@gmail.com>
2025-10-06 17:05:12 +0100

    setup tanstack in project

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import router from "./router.js";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root") as HTMLDivElement).render(
-	<StrictMode>
-		<RouterProvider router={router} />
-	</StrictMode>
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </StrictMode>
 );
