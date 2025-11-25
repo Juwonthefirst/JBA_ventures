55b994dacae18f72d501199011b3cad9d9c155a9
juwonthefirst <ajibolajuwon5@gmail.com>
2025-11-25 07:58:28 +0100

    increased api request timeout

--- src/api-client.ts
+++ src/api-client.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: String(import.meta.env.VITE_BACKEND_URL),
-  timeout: 3000,
+  timeout: 5000,
   withCredentials: true,
 });
